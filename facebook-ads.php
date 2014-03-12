fca52e8e8536c45626d7b5d366b4326a602cafef
alitaj <alitaj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-12 23:02:16 +0000

    changed wording
    git-svn-id: https://plugins.svn.wordpress.org/facebook-ads/trunk@874396 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- facebook-ads.php
+++ facebook-ads.php
@@ -2,7 +2,7 @@
   /**
    * Plugin Name: Facebook Ads
    * Plugin URI: http://corp.wishpond.com/facebook-ads-tool/
-   * Description: This plugin lets you create Ads on Facebook directly from your Wordpress site. Promote your posts and pages on Facebook and drive new customers.
+   * Description: This plugin lets you create Ads on Facebook from your Wordpress site. Promote your posts and pages on Facebook and drive new customers.
    * Version: 1.1
    * Author: Wishpond
    * Text Domain: facebook-ads
